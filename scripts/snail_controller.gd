13b6220623f1fe20fc26dd1e02ce56f3feed88cc
Russell Nairn <74921709+RNairn01@users.noreply.github.com>
2022-10-01 01:35:15 +0100

    Add basic collision detection

--- scripts/snail_controller.gd
+++ scripts/snail_controller.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends Area2D
 
 var tile_size = 32
 onready var timer = get_node("/root/Node2D/GlobalTimer")
@@ -8,10 +8,15 @@ export var snail_direction: Vector2 = Vector2.RIGHT
 
 func _ready():
 	timer.connect("timeout", self, "_on_tick")
+	self.connect("area_entered", self, "_on_collide")
 
 func _on_tick():
 	print("tick")
-	position += snail_speed * snail_direction * tile_size
+	var move_target = snail_speed * snail_direction * tile_size
+	position += move_target
+
+func _on_collide(area: Area2D):
+	print("collide" + "-" + area.name)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

e8e3789ecb39d5dd4ab10241505a80bd79d970b6
Russell Nairn <74921709+RNairn01@users.noreply.github.com>
2022-10-01 23:29:05 +0100

    Add random snail generation

--- scripts/snail_order_manager.gd
+++ scripts/snail_order_manager.gd
@@ -7,6 +7,8 @@ var rebel_snail = preload("res://scenes/snails/snail_rebel.tscn")
 onready var timer_label = get_node("/root/Node2D/UI/SidePanel/TimerLabel")
 onready var next = snail_order.pop_front().instance()
 
+var rng = RandomNumberGenerator.new()
+
 signal next_snail
 
 var snail_order = [normal_snail]
@@ -18,10 +20,13 @@ var tutorial_snail_order = [
 ]
 
 func _ready():
+	rng.randomize()
 	timer_label.connect("timeout_label", self, "_on_timeout")
 
 	#Will update this when we add no tutorial option
-	snail_order = tutorial_snail_order.duplicate(true)
+	# snail_order = tutorial_snail_order.duplicate(true)
+	if snail_order.size() < 5:
+		generate_more_snails()
 
 func _on_timeout():
 	next = snail_order.pop_front().instance()
@@ -31,7 +36,15 @@ func _on_timeout():
 
 func generate_more_snails():
 	for _i in range(1,20):
-		snail_order.push(random_snail())
+		snail_order.append(random_snail())
 
 func random_snail():
-	return normal_snail
+	var random_n = rng.randi_range(1,100)
+	if random_n < 51:
+		return normal_snail
+	elif random_n < 71:
+		return speedy_snail
+	elif random_n < 91:
+		return big_snail
+	else:
+		return rebel_snail

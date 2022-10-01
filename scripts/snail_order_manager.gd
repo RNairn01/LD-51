2a8670c15bd74b2f78dfada0412c15b57ebe5f0c
Russell Nairn <74921709+RNairn01@users.noreply.github.com>
2022-10-01 23:12:02 +0100

    Add placeholder generating of new snails

--- scripts/snail_order_manager.gd
+++ scripts/snail_order_manager.gd
@@ -5,10 +5,11 @@ var speedy_snail = preload("res://scenes/snails/snail_speedy.tscn")
 var big_snail = preload("res://scenes/snails/snail_big.tscn")
 var rebel_snail = preload("res://scenes/snails/snail_rebel.tscn")
 onready var timer_label = get_node("/root/Node2D/UI/SidePanel/TimerLabel")
-onready var next = tutorial_snail_order.pop_front().instance()
+onready var next = snail_order.pop_front().instance()
 
 signal next_snail
 
+var snail_order = [normal_snail]
 var tutorial_snail_order = [
 	normal_snail, normal_snail, normal_snail, normal_snail, normal_snail, 
 	speedy_snail, normal_snail, normal_snail, speedy_snail, normal_snail,
@@ -19,6 +20,18 @@ var tutorial_snail_order = [
 func _ready():
 	timer_label.connect("timeout_label", self, "_on_timeout")
 
+	#Will update this when we add no tutorial option
+	snail_order = tutorial_snail_order.duplicate(true)
+
 func _on_timeout():
-	next = tutorial_snail_order.pop_front().instance()
+	next = snail_order.pop_front().instance()
 	emit_signal("next_snail", next.name)
+	if snail_order.size() < 5:
+		generate_more_snails()
+
+func generate_more_snails():
+	for _i in range(1,20):
+		snail_order.push(random_snail())
+
+func random_snail():
+	return normal_snail

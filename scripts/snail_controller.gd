4be2c4ae274f9aeffd4c3c733366865f81c97ee4
Russell Nairn <74921709+RNairn01@users.noreply.github.com>
2022-10-01 08:42:54 +0100

    Add background grid, basic snail sprite and direction changing of sprite

--- scripts/snail_controller.gd
+++ scripts/snail_controller.gd
@@ -3,6 +3,7 @@ extends Area2D
 var tile_size = 32
 onready var timer = get_node("/root/Node2D/GlobalTimer")
 onready var tween = $Tween
+onready var sprite = $SnailSprite
 export var tween_speed = 3
 export var snail_speed = 1
 export var snail_size = 1
@@ -10,21 +11,34 @@ export var snail_direction: Vector2 = Vector2.RIGHT
 
 func _ready():
 	timer.connect("timeout", self, "_on_tick")
-    # This could be done in the editor but I think it's more descriptive in code.
+	# This could be done in the editor but I think it's more descriptive in code.
 	self.connect("area_entered", self, "_on_collide")
 
 func _on_tick():
 	print("tick")
 	var move_target = position + snail_speed * snail_direction * tile_size
+	set_sprite_facing(snail_direction)
 	move_tween(move_target)
 
 func _on_collide(area: Area2D):
 	print("collide" + "-" + area.name)
 
+func set_sprite_facing(direction: Vector2):
+	match (direction):
+		Vector2.UP:
+			sprite.rotation_degrees = 90
+		Vector2.DOWN:
+			sprite.rotation_degrees = -90
+		Vector2.RIGHT:
+			sprite.flip_h = true 
+		Vector2.LEFT:
+			sprite.flip_h = false 
+
+
 func move_tween(target):
 	tween.interpolate_property(self, "position",
 		position, target,
-		1.0/snail_speed, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+		1.0/tween_speed, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
 	tween.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
